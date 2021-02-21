a2ba5caeb8dfd7347aa8e7e8aa89c424a3e58c37
Junling <junlingsun1983@gmail.com>
2021-02-21 11:12:12 -0500

    add and config gateway

--- online_mall_gateway/src/main/java/com/junling/online_mall/gateway/OnlineMallGatewayApplication.java
+++ online_mall_gateway/src/main/java/com/junling/online_mall/gateway/OnlineMallGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.junling.online_mall.product;
+package com.junling.online_mall.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class OnlineMallProductApplication {
+public class OnlineMallGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlineMallProductApplication.class, args);
+		SpringApplication.run(OnlineMallGatewayApplication.class, args);
 	}
 
 }
---
+++ online_mall_gateway/src/test/java/com/junling/online_mall/gateway/OnlineMallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junling.online_mall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMallGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
